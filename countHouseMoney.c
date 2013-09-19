9c9e286778f5a78d6a58efafbcb2f43f86414aad
kamalarieff <kamal.arieff@gmail.com>
2013-09-20 01:56:18 +0930

    update the library used

--- countHouseMoney.c
+++ countHouseMoney.c
@@ -1,11 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 typedef struct {
 	float moneyUsed,moneyPay;
